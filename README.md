0715ae335f1fe7f51770aac20158cdc867abdc92
noto173 <warreneverettstevens@gmail.com>
2026-05-22 18:29:26 -0400

    reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-`sudo apt install qemu-system-x86 fasm dosfstools mtools`<br>
+`sudo apt install qemu-system-x86 fasm dosfstools mtools fatsort`<br>
 the pc speaker is ear exploding PLEASE turn down your volume if running a program using it<br>
 `bash build.sh test`<br>
 requires a 386+ CPU, standard VGA output, 1 full MiB of RAM
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
-rm -f os.img,other.img
+rm -f os.img
 mkdir -p build
 
 fasm src/boot/boot.asm build/boot.bin
@@ -26,17 +26,13 @@ fasm src/userspace/echo.asm build/echo.com
 fasm src/tunes/mouth.asm build/mouth.tun
 fasm src/tunes/scale.asm build/scale.tun
 fasm src/tunes/pb95.asm build/pb95.tun
-fasm src/tunes/852.asm build/852.tun
+fasm src/tunes/eight52.asm build/eight52.tun
 
 # gcc -m16 -ffreestanding -nostdlib -fno-pie -fno-pic -Wl,--oformat=binary -s -o build/ctest.bin src/ctest.c
 
 touch os.img
 truncate -s 1440k os.img
 mkfs.fat -n STANNUM -F 12 -f 1 os.img
-
-touch other.img
-truncate -s 1440k other.img
-mkfs.fat -n DATA -F 12 -f 1 other.img
 
 dd if=build/boot.bin of=os.img count=3 bs=1 conv=notrunc status=none
 dd if=build/boot.bin of=os.img seek=72 skip=72 count=440 bs=1 conv=notrunc status=none
@@ -54,30 +50,33 @@ mcopy -i os.img build/write.com "::write.com"
 mcopy -i os.img build/copyf.com "::copyf.com"
 mcopy -i os.img build/echo.com "::echo.com"
 mcopy -i os.img build/vesatest.com "::vesatest.com"
+mcopy -i os.img build/beeper.com "::beeper.com"
+mcopy -i os.img build/video12.com "::video12.com"
+mcopy -i os.img build/video13.com "::video13.com"
+mcopy -i os.img build/image.com "::image.com"
+mcopy -i os.img build/panick.com "::panick.com"
 
 mcopy -i os.img src/userspace/config/config.sys "::config.sys"
 mcopy -i os.img src/userspace/config/usr.cfg "::usr.cfg"
 
 mcopy -i os.img LICENSE "::license.txt"
+mcopy -i os.img src/userspace/docs/abc.txt "::abc.txt"
+mcopy -i os.img spec/extensions.txt "::extens.txt"
 
-mcopy -i other.img build/beeper.com "::beeper.com"
-mcopy -i other.img build/video12.com "::video12.com"
-mcopy -i other.img build/video13.com "::video13.com"
-mcopy -i other.img build/image.com "::image.com"
-mcopy -i other.img build/panick.com "::panick.com"
+mcopy -i os.img build/mouth.tun "::mouth.tun"
+mcopy -i os.img build/scale.tun "::scale.tun"
+mcopy -i os.img build/pb95.tun "::pb95.tun"
+mcopy -i os.img build/eight52.tun "::eight52.tun"
 
-mcopy -i other.img src/userspace/docs/abc.txt "::abc.txt"
-mcopy -i other.img spec/extensions.txt "::extens.txt"
+mcopy -i os.img assets/scp079.raw "::scp079.raw"
+mcopy -i os.img assets/maroi.raw "::maroi.raw"
+mcopy -i os.img assets/listen.raw "::listen.raw"
+mcopy -i os.img assets/franklin.raw "::franklin.raw"
 
-mcopy -i other.img build/mouth.tun "::mouth.tun"
-mcopy -i other.img build/scale.tun "::scale.tun"
-mcopy -i other.img build/pb95.tun "::pb95.tun"
-mcopy -i other.img build/852.tun "::852.tun"
-
-mcopy -i other.img assets/079.raw "::079.raw"
+fatsort os.img
 
 if [[ "$1" == "test" ]]; then
-    qemu-system-x86_64 -name Stannum -fda os.img -fdb other.img -machine pcspk-audiodev=spk -audiodev pa,id=spk -vga std --enable-kvm -cpu host
+    qemu-system-x86_64 -name Stannum -fda os.img -machine pcspk-audiodev=spk -audiodev pa,id=spk -vga std --enable-kvm -cpu host
 else
     echo "[build.sh] Run \"bash build.sh test\" to build and then boot into qemu (qemu-system-x86 package required)"
 fi
