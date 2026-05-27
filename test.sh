4073b70022d7dc347557512244fc56f1f0503cab
noto173 <warreneverettstevens@gmail.com>
2026-05-26 20:12:31 -0400

    remove kvm because it actually is just slower actually

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-qemu-system-x86_64 -name Stannum -drive file=os.img,if=floppy,format=raw -machine pcspk-audiodev=spk -audiodev pa,id=spk -vga std --enable-kvm -cpu host
+qemu-system-x86_64 -name Stannum -drive file=os.img,if=floppy,format=raw -machine pcspk-audiodev=spk -audiodev pa,id=spk -vga std # --enable-kvm -cpu host
